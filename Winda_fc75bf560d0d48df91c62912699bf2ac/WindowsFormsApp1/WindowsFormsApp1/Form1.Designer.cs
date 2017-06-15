952e0f38b2ddc62260c75914a4cf1fb0f75616d6
Anna Kadziera <akadziera@onet.pl>
2017-06-15 16:02:34 +0200

    5seconds waiting option added/lists of passengers

--- Winda_fc75bf560d0d48df91c62912699bf2ac/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Winda_fc75bf560d0d48df91c62912699bf2ac/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -32,15 +32,22 @@
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(738, 264);
+            this.button1.Location = new System.Drawing.Point(759, 29);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "First floor";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_View);
@@ -52,25 +59,90 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(759, 161);
+            this.button2.Location = new System.Drawing.Point(759, 58);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Second Floor";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 94);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(759, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(759, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(852, 337);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_View);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,6 +151,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Winda_fc75bf560d0d48df91c62912699bf2ac/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Winda_fc75bf560d0d48df91c62912699bf2ac/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
@@ -13,17 +14,36 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+       
+        private int i;
+        private int j;
         private int x;
         private int y;
         private int z;
+       
+        private string mytext;
         private int pietropierwsze;
         private int pietrodrugie;
+        private int normalweight = 70;
+        private int maxweight = 600;
+        public struct Passenger
+        {
+            public int id;
+            public int weight;
+
+        }
+
+        List<int> passenger1 = new List<int>();
+        List<int> passenger2 = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
             z = 50;
             x = 300;
             y = 50;
+            i = 0;
+            j = 0;
         }
 
         private void Form1_View(object sender, PaintEventArgs e) {
@@ -34,31 +54,18 @@ namespace WindowsFormsApp1
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            if (i == 0)
+            {
+                pietropierwsze = x;
 
-            /*  try
-              {
-                  Bitmap image1 = (Bitmap)Image.FromFile(
-                      @"C:\Użytkownicy\Józef\Pulpit\1.bmp", true);
-
-                  TextureBrush texture = new TextureBrush(image1);
-                  texture.WrapMode = System.Drawing.Drawing2D.WrapMode.Tile;
-                  Graphics formGraphics = this.CreateGraphics();
-                  formGraphics.FillEllipse(texture,
-                      new RectangleF(90.0F, 110.0F, 100, 100));
-                  formGraphics.Dispose();
-
-              }
-              catch (System.IO.FileNotFoundException)
-              {
-                  MessageBox.Show("There was an error opening the bitmap." +
-                      "Please check the path.");
-              }
-              */
-
-            pietropierwsze = 200;
-            pietrodrugie = 190;
-            timer1_Tick(sender, e);
-
+            }
+            else
+            {  
+                System.Threading.Thread.Sleep(5000);
+                pietropierwsze = 200;
+                pietrodrugie = 190;
+                timer1_Tick(sender, e);
+            }
 
         }
        
@@ -70,7 +77,7 @@ namespace WindowsFormsApp1
                 y += 10;
                 z += 10;
             }
-            else if (z==pietrodrugie && y>=pietropierwsze && y<=pietropierwsze)
+            else if (z==pietrodrugie && y>=100 && y<=pietropierwsze)
             {   
                 y=y-10;
                 if (y == 100)
@@ -87,6 +94,39 @@ namespace WindowsFormsApp1
             pietropierwsze = 400;
             pietrodrugie = 390;
             timer1_Tick(sender, e);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           // Image myImg = Image.FromFile(@"1.jpg");
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            passenger1.Add(i);
+            i++;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show(i.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           passenger1.RemoveAt(1);
         }
     }
 }
